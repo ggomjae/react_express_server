a0cacb83a5d5945649fc5514b04414bab865a37e
ggomjae <ggomjae@naver.com>
2020-12-22 17:38:50 +0900

    react 설치:  react_express_server - fronted + server

--- server/express/server.js
+++ server/express/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const port = 3000;
+const port = 3001;
 
 // DB
 const dbConfig = require('./config/database')
