28ffd2be198f788b7286a6f58d7c63080f586235
ggomjae <ggomjae@naver.com>
2021-01-21 20:38:36 +0900

    Error Page 추가

---
+++ fronted/src/components/views/ErrorPage/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div className="container">
+      <h2 className="title">요청하신 페이지를 찾을 수 없습니다.</h2>
+      <div>
+        <p> 404 ERROR </p>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
