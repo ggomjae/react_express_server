f72d8fc45e07590e89b1ad58a024fc91d952ffd5
ggomjae <ggomjae@naver.com>
2021-01-22 19:38:49 +0900

    ErrorPage SCSS 추가

--- fronted/src/components/views/ErrorPage/ErrorPage.js
+++ fronted/src/components/views/ErrorPage/ErrorPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './ErrorPage.scss';
 
 function ErrorPage() {
   return (
-    <div className="container">
+    <div className="error_page_wrap">
+      <div className="container">
       <h2 className="title">요청하신 페이지를 찾을 수 없습니다.</h2>
       <div>
         <p> 404 ERROR </p>
       </div>
+    </div>
     </div>
   );
 }
